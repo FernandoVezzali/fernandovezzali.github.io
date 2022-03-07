a827c10b687b427db4d35d619a86dd36160a1cce
Fernando Ayrosa Vezzali <fvezzali@questrade.com>
2022-03-07 17:20:29 -0300

    docsaurus

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('C:\\projects\\fv\\vezzali\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\projects\\fv\\vezzali\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\projects\\fv\\vezzali\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css'),
-  require('C:\\projects\\fv\\vezzali\\src\\css\\custom.css'),
-];
