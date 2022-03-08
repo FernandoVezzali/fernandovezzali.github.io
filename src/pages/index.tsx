2d49fafce9631972244e1310ab80fa1a598052d4
FernandoVezzali <fernandovezzali@gmail.com>
2022-03-08 08:28:00 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Unit Testing in CSharp - 15min ⏱️
           </Link>
         </div>
       </div>
