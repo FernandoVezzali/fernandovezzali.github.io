f6c73417b4d64c4c2b835595de9ed043160690a8
Fernando Ayrosa Vezzali <fernando.vezzali@vanco.com>
2025-03-20 17:00:20 -0300

    add page

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,6 +3,41 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
+    path: '/__docusaurus/debug',
+    component: ComponentCreator('/__docusaurus/debug', '0c0'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/config',
+    component: ComponentCreator('/__docusaurus/debug/config', 'b0e'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/content',
+    component: ComponentCreator('/__docusaurus/debug/content', '481'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/globalData',
+    component: ComponentCreator('/__docusaurus/debug/globalData', '8ee'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/metadata',
+    component: ComponentCreator('/__docusaurus/debug/metadata', '1a1'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/registry',
+    component: ComponentCreator('/__docusaurus/debug/registry', '22b'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/routes',
+    component: ComponentCreator('/__docusaurus/debug/routes', '6f4'),
+    exact: true
+  },
+  {
     path: '/blog',
     component: ComponentCreator('/blog', '85c'),
     exact: true
@@ -64,11 +99,17 @@ export default [
   },
   {
     path: '/docs',
-    component: ComponentCreator('/docs', 'fe8'),
+    component: ComponentCreator('/docs', 'b8f'),
     routes: [
       {
         path: '/docs/behavioral-design-patterns/strategy-patterns',
         component: ComponentCreator('/docs/behavioral-design-patterns/strategy-patterns', '4b7'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/docs/bookmark',
+        component: ComponentCreator('/docs/bookmark', 'f35'),
         exact: true,
         sidebar: "tutorialSidebar"
       },
